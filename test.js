86336dad34e12d66131ab39752382fc43846fc23
Poorvi Agarwal <poorviagarwal.cse22@jecrc.ac.in>
2021-01-12 16:29:35 +0530

    just for testing

---
+++ test.js
@@ -0,0 +1,20 @@
+/* var p=prompt("Enter password");
+ if(p=="poorvi")
+
+{alert("hello poorvi");
+
+}
+else
+{
+	alert("bye");
+}
+var a=confirm("is poorvi good");
+if(a){
+	alert("yes she is");
+}
+else{
+	alert("awwww");}
+//document.write(p);*/
+function count()
+{
+
